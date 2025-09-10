e7286ac5f3f4442357ef8192948a6be8e5dc3042
Gragy08 <giaqui1712@gmail.com>
2025-09-10 22:17:20 +0700

    Làm trang Thêm mới danh mục bài viết

--- index.ts
+++ index.ts
@@ -3,8 +3,17 @@ import path from 'path';
 import adminRoutes from "./routes/admin/index.route";
 import clientRoutes from "./routes/client/index.route";
 import { pathAdmin } from './configs/variable.config';
+import dotenv from 'dotenv';
+import { connectDB } from './configs/database.config';
+
+// Load biến môi trường từ file .env
+dotenv.config();
+
 const app = express();
 const port = 3000;
+
+// Kết nối đến cơ sở dữ liệu
+connectDB();
 
 // Thiết lập thư mục views và view engine Pug
 app.set('views', path.join(__dirname, 'views')); // Thư mục chứa file Pug
@@ -22,3 +31,5 @@ app.use("/", clientRoutes);
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`);
 });
+
+//giaqui1712_db_user / mIbSvkvF9e3QzkXx
