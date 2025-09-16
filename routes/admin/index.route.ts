1525471e24f6875339ad0adfde81e4627b6fb850
Gragy08 <giaqui1712@gmail.com>
2025-09-16 20:00:54 +0700

    Làm trang Thêm mới danh mục bài viết (Tiếp) - Thêm slug

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import dashboardRoutes from "./dashboard.route";
 import articleRoutes from "./article.route";
+import helperRoutes from "./helper.route";
 
 const router = Router();
 
 router.use('/dashboard', dashboardRoutes);
 router.use('/article', articleRoutes);
+router.use('/helper', helperRoutes);
 
 export default router;
