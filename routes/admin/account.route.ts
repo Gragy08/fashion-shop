8c2fdaf0c644900166c55716003ec502d502b9ed
Gragy08 <giaqui1712@gmail.com>
2025-11-21 15:35:48 +0700

    Đăng nhập bên admin

---
+++ routes/admin/account.route.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import * as accountController from "../../controllers/admin/account.controller";
+import multer from "multer";
+import * as accountValidate from "../../validates/admin/account.validate";
+
+const router = Router();
+
+const upload = multer();
+
+router.get('/login', accountController.login);
+
+router.post(
+  '/login', 
+  upload.none(),
+  accountValidate.loginPost,
+  accountController.loginPost
+);
+
+export default router;
