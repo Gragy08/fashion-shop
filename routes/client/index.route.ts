192b921d339efc52cf505aa0925fe7e993a03a2f
Gragy08 <giaqui1712@gmail.com>
2025-12-02 20:21:11 +0700

    Tạo khung giao diện

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import homeRoutes from "./home.route";
 import articleRoutes from "./article.route";
+import productRoutes from "./product.route";
 import * as categoryMiddleware from "../../middlewares/client/category.middleware";
 
 const router = Router();
@@ -10,5 +11,7 @@ router.use(categoryMiddleware.getAllCategory);
 router.use('/', homeRoutes);
 
 router.use('/article', articleRoutes);
+
+router.use('/product', productRoutes);
 
 export default router;
